413bde442f185704982f159f40075cf3d36c4e30
tatamartinsss <martins95coin@gmail.com>
2022-04-13 14:58:56 +0200

    fix remaining

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,6 +3,7 @@ import * as anchor from '@project-serum/anchor';
 import { MintCountdown } from './MintCountdown';
 import { toDate, formatNumber } from '../utils/utils';
 import { CandyMachineAccount } from '../utils/candy-machine';
+import { useEffect, useState } from 'react';
 
 export const Header = ({
     candyMachine,
@@ -11,13 +12,28 @@ export const Header = ({
     candyMachine?: CandyMachineAccount;
     refreshCandyMachineState: any;
 }) => {
+    const [itemsRemaining, setItemsRemaining] = useState<number>();
+    useEffect(() => {
+        // amount to stop the mint?
+        if (candyMachine?.state.endSettings?.endSettingType.amount) {
+            let limit = Math.min(candyMachine.state.endSettings.number.toNumber(), candyMachine.state.itemsAvailable);
+            if (candyMachine.state.itemsRedeemed < limit) {
+                setItemsRemaining(limit - candyMachine.state.itemsRedeemed);
+            } else {
+                setItemsRemaining(0);
+                candyMachine.state.isSoldOut = true;
+            }
+        } else {
+            setItemsRemaining(candyMachine?.state.itemsRemaining);
+        }
+    }, []);
     return (
         <div className="flex flex-row flex-nowrap end">
             {candyMachine && (
                 <>
                     <div className="flex flex-col justify-center text-center align-middle flex-nowrap basis-1/4">
                         <p className="m-0 text-sm font-normal tracking-wide text-neutral-300">Remaining</p>
-                        <p className="m-0 text-xl font-semibold tracking-wide text-white">{`${candyMachine?.state.itemsRemaining}`}</p>
+                        <p className="m-0 text-xl font-semibold tracking-wide text-white">{`${itemsRemaining}`}</p>
                     </div>{' '}
                     <div className="flex flex-col justify-center text-center align-middle flex-nowrap basis-1/4">
                         <p className="m-0 text-sm font-normal tracking-wide text-neutral-300">Price</p>
